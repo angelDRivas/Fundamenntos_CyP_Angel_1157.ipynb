893e3019d6f8aae65b3d90212ae6bd8bd3d2dd96
angelDRivas <90007590+angelDRivas@users.noreply.github.com>
2021-10-12 18:42:14 -0500

    Update and rename Fundamentos_CyP_1157.ipynb to Fundamentos_CyP_1157.py

---
+++ Fundamentos_CyP_1157.py
@@ -0,0 +1,12 @@
+#declaracion de variable
+nombre="Angel"
+carrera="ingenieria en computacion"
+frase ="Hola mundo"
+print(nombre)
+print(carrera)
+print(frase)
+#type devuelve el tipo de dato
+print(type (nombre))
+paterno= str ("Rivas")
+print(paterno)
+
