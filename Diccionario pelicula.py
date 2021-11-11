602b9d62aa579319889010e2db450253d8661e6b
angelDRivas <90007590+angelDRivas@users.noreply.github.com>
2021-11-11 16:47:29 -0600

    Create Diccionario pelicula.py

---
+++ Diccionario pelicula.py
@@ -0,0 +1,9 @@
+pelicula= { "titulo":"Harry potter y la piedra filosofal",
+           "duracion":"1 hora y 40 minutos",
+           "personajes_principales":{
+               "Harry potter"
+              
+           }}
+
+
+print(pelicula["titulo"])
