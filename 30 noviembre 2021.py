82b350338a10d4c5370f43d811ad3082ec496887
angelDRivas <90007590+angelDRivas@users.noreply.github.com>
2021-11-30 17:51:17 -0600

    Create 30 noviembre 2021.py

---
+++ 30 noviembre 2021.py
@@ -0,0 +1,29 @@
+valor = int (input("Dame un valor entero"))
+while valor != 0:
+    print(valor)
+    print("otras cosas")
+print("Fin del programa")
+
+comprobar = True
+while comprobar = True
+n = int(input("Ingrese un numero entero positivo:"))
+if n>0:
+    comprobar = False
+    resultado = 0
+    for i in range(10):
+        resultado +=(1*i)
+        print("el resultado de la serie es:", resultado)
+else:
+    print("el numero ingresado no es correcto")
+    
+comprobar = True
+while comprobar = True
+n = int(input("Ingrese un numero entero positivo:"))
+if n>0:
+    comprobar = False
+    resultado = 0
+    for i in range(1, n+ 1):
+        resultado +=(1/i)
+        print("el resultado de la serie es:", resultado)
+else:
+    print("el numero ingresado no es correcto")
